8c8ff5fd8298be97c2de553e1fd9944116c07dab
Susan56789 <devnimoh@gmail.com>
2024-05-13 12:08:39 +0300

    LOGIN/SIGN UP FORMS

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -10,30 +10,86 @@ const LoginForm = ({ handleLogin }) => {
   };
 
   return (
-    <div className="container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+    <section className="vh-100">
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-9 col-lg-6 col-xl-5">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              className="img-fluid"
+              alt="Sample"
+            />
+          </div>
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <form onSubmit={handleSubmit}>
+              <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                <p className="lead fw-normal mb-0 me-3">Sign in with</p>
+                <button type="button" className="btn btn-primary btn-floating mx-1">
+                  <i className="fab fa-facebook-f"></i>
+                </button>
+
+                <button type="button" className="btn btn-primary btn-floating mx-1">
+                  <i className="fab fa-twitter"></i>
+                </button>
+
+                <button type="button" className="btn btn-primary btn-floating mx-1">
+                  <i className="fab fa-linkedin-in"></i>
+                </button>
+              </div>
+
+              <div className="divider d-flex align-items-center my-4">
+                <p className="text-center fw-bold mx-3 mb-0">Or</p>
+              </div>
+
+              {/* Email input */}
+              <div className="form-outline mb-4">
+                <input
+                  type="email"
+                  id="email"
+                  className="form-control form-control-lg"
+                  placeholder="Enter a valid email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <label className="form-label" htmlFor="email">Email address</label>
+              </div>
+
+              {/* Password input */}
+              <div className="form-outline mb-3">
+                <input
+                  type="password"
+                  id="password"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="form-label" htmlFor="password">Password</label>
+              </div>
+
+              <div className="d-flex justify-content-between align-items-center">
+                {/* Checkbox */}
+                <div className="form-check mb-0">
+                  <input className="form-check-input me-2" type="checkbox" value="" id="rememberMe" />
+                  <label className="form-check-label" htmlFor="rememberMe">
+                    Remember me
+                  </label>
+                </div>
+                <a href="#!" className="text-body">Forgot password?</a>
+              </div>
+
+              <div className="text-center text-lg-start mt-4 pt-2">
+                <button type="submit" className="btn btn-primary btn-lg" style={{ paddingLeft: '2.5rem', paddingRight: '2.5rem' }}>
+                  Login
+                </button>
+                <p className="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!" className="link-danger">Register</a></p>
+              </div>
+
+            </form>
+          </div>
         </div>
-        <div className="mb-3">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 }
 
--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -11,39 +11,97 @@ const RegisterForm = ({ handleRegister }) => {
   };
 
   return (
-    <div className="container">
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+    <section>
+      <div className="px-4 py-5 px-md-5 text-center text-lg-start" style={{ backgroundColor: 'hsl(0, 0%, 96%)' }}>
+        <div className="container">
+          <div className="row gx-lg-5 align-items-center">
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <h1 className="my-5 display-3 fw-bold ls-tight">
+                The best offer <br />
+                <span className="text-primary">for your business</span>
+              </h1>
+              <p style={{ color: 'hsl(217, 10%, 50.8%)' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Eveniet, itaque accusantium odio, soluta, corrupti aliquam
+                quibusdam tempora at cupiditate quis eum maiores libero
+                veritatis? Dicta facilis sint aliquid ipsum atque?
+              </p>
+            </div>
+
+            <div className="col-lg-6 mb-5 mb-lg-0">
+              <div className="card">
+                <div className="card-body py-5 px-md-5">
+                  <form onSubmit={handleSubmit}>
+
+                    <div className="row">
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <input
+                            type="text"
+                            id="username"
+                            className="form-control"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                          />
+                          <label className="form-label" htmlFor="username">Username</label>
+                        </div>
+                      </div>
+                      <div className="col-md-6 mb-4">
+                        <div className="form-outline">
+                          <input
+                            type="email"
+                            id="email"
+                            className="form-control"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                          />
+                          <label className="form-label" htmlFor="email">Email address</label>
+                        </div>
+                      </div>
+                    </div>
+
+
+                    <div className="form-outline mb-4">
+                      <input
+                        type="password"
+                        id="password"
+                        className="form-control"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      <label className="form-label" htmlFor="password">Password</label>
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-block mb-4">
+                      Sign up
+                    </button>
+
+                    <div className="text-center">
+                      <p>or sign up with:</p>
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <i className="fab fa-facebook-f"></i>
+                      </button>
+
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <i className="fab fa-google"></i>
+                      </button>
+
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <i className="fab fa-twitter"></i>
+                      </button>
+
+                      <button type="button" className="btn btn-link btn-floating mx-1">
+                        <i className="fab fa-github"></i>
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="mb-3">
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">Register</button>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 }
 
